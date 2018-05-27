cd637cf9020c86f2470cd03f9b1080897bd9b81a
Michael Franklin <boo@illusional.net>
2018-05-28 05:51:50 +1000

    Adds more detail to About Page

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -5,8 +5,13 @@ class AboutPage extends React.Component {
     render() {
         return (
             <div style={{padding: "10px"}}>
-                <Typography>About</Typography>
-                <Typography>This application uses React, React-Router and was built based on a tutorial from PuralSight</Typography>
+                <Typography variant="headline" align="center" gutterBottom>About this Application</Typography>
+
+                <Typography>This application estimates the road surface area of a given set of bounds.</Typography>
+                <Typography>You can change these bounds by scrolling the map and clicking the RELOAD MAP AND DATA button.</Typography>
+                <Typography>All values are estimated based on OpenStreetMap data.</Typography>
+                <Typography>This interface uses some of the following React modules: Material-UI, React-Leaflet, react-places-autocomplete</Typography>
+                <Typography>This project was developed by Michael Franklin for FIT3036 (Computer Science Project), supervised by Dr. Rasika Amarasiri.</Typography>
             </div>
         );
     }
